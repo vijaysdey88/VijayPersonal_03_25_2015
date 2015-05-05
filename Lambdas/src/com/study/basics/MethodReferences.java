f3b77e89dcd43f7f49da493363b5f2a123814abe
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-05 18:31:54 +0530

    Added test for constructor reference

--- Lambdas/src/com/study/basics/MethodReferences.java
+++ Lambdas/src/com/study/basics/MethodReferences.java
@@ -2,6 +2,8 @@ package com.study.basics;
 
 import org.junit.Test;
 
+import com.study.models.Person;
+
 public class MethodReferences {
 
 	public static int addInt(int x, int y) {
@@ -25,7 +27,13 @@ public class MethodReferences {
 		Util util = new Util();
 		Adder<String> strAdder = util::addStr;
 		System.out.println(strAdder.add("a", "b"));
-
+	}
+	
+	@Test
+	public void useContructorReference() {
+		PersonFactory factory = Person::new;
+		
+		System.out.println(factory.create("Vijay", 25));
 	}
 
 }
