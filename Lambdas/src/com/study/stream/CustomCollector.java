89514b237f264169b9e533293d595bce51b8d098
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-11 12:44:05 +0530

    Streams collectors use a custom container

--- Lambdas/src/com/study/stream/CustomCollector.java
+++ Lambdas/src/com/study/stream/CustomCollector.java
@@ -12,19 +12,26 @@ import com.study.models.Person;
 
 public class CustomCollector {
 	
-private List<Person> persons;
-
-private Collector<Person, StringJoiner, String> formattedPersonCollector = 
-															Collector.of(
-																	() ->  {												//Supplier
-																			 System.out.println("Inside supplier...");
-																			 return new StringJoiner(" | "); },				 
-																	(c, p) -> c.add(p.getName() + ":" + p.getAge()), 		//Accumulator
-																	(c1, c2) -> {											//Combiner
-																					System.out.println("Inside combiner...");
-																					return c1.merge(c2);
-																				}, 	   
-																	(c) -> c.toString());		  							 //Finisher
+	private List<Person> persons;
+	
+	private Collector<Person, StringJoiner, String> formattedPersonCollector = 
+																Collector.of(
+																		() ->  {												//Supplier
+																				 System.out.println("Inside supplier...");
+																				 return new StringJoiner(" | "); },				 
+																		(c, p) -> c.add(p.getName() + ":" + p.getAge()), 		//Accumulator
+																		(c1, c2) -> {											//Combiner
+																						System.out.println("Inside combiner...");
+																						return c1.merge(c2);
+																					}, 	   
+																		(c) -> c.toString());		  							 //Finisher
+	
+	private Collector<Person, MaxContainer, Person> eldestPerson = 
+														Collector.of(
+																MaxContainer::new,
+																MaxContainer::add,
+																MaxContainer::merge, 
+																MaxContainer::getResult);
 
 	
 	@Before
@@ -49,6 +56,37 @@ private Collector<Person, StringJoiner, String> formattedPersonCollector =
 									.parallelStream()
 									.collect(formattedPersonCollector);
 		System.out.println("Formatted string - " + formattedString);
+	}
+	
+	
+	@Test
+	public void useCustomerCollectorWithCustomContainerToFindMaxAge() {
+		Person eldest = persons
+							.stream()
+							.collect(eldestPerson);
+		System.out.println("Eldest - " + eldest);
+			
+	}
+	
+	private static class MaxContainer {
+		private Person eldest = null;
+		
+		private void add(Person p) {
+			if(null == eldest || p.getAge() > eldest.getAge()) {
+				eldest = p;
+			}
+		}
+		
+		private MaxContainer merge(MaxContainer other) {
+			if(other.eldest.getAge() > this.eldest.getAge()) {
+				this.eldest = other.eldest;
+			}
+			return this;
+		}
+		
+		private Person getResult() {
+			return eldest;
+		}
 	}
 	
 }
