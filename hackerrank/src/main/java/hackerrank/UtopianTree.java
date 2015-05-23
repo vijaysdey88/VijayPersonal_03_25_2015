677a63f6fbedd2e9119a5b17285f84a9d8808eef
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-23 11:02:00 +0530

    Utopian tree java 8 solution

--- hackerrank/src/main/java/hackerrank/UtopianTree.java
+++ hackerrank/src/main/java/hackerrank/UtopianTree.java
@@ -1,18 +1,26 @@
 package hackerrank;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class UtopianTree {
+	private Predicate<Integer> isSpring = cycle -> cycle % 2 != 0; 
+	private Function<Integer, Function<Integer, Integer>> getHeightComputer =
+				(cycle) -> {
+					if(isSpring.test(cycle)) {
+						return current -> current * 2;
+					} else {
+						return current -> current + 1;
+					}
+				};
 
 	public int projectedHeight(int cycles) {
-		int height = 0;
-		for (int i = 0; i <= cycles; i++) {
-			if (i % 2 != 0) {
-				height += height;
-			} else {
-				height += 1;
-			}
-		}
-		return height;
+		return IntStream
+				.range(0, cycles + 1)
+				.reduce(0, (height, cycle) -> {
+							return getHeightComputer.apply(cycle)
+													.apply(height);
+				});
 	}
-
 }
