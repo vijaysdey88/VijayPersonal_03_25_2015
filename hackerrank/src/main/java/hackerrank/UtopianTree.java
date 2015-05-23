d3a10ffeaeb284e6463cbcbb8f33b916c7d30630
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-23 11:27:03 +0530

    Utopian tree make solution expandable for more types of cycles using map

--- hackerrank/src/main/java/hackerrank/UtopianTree.java
+++ hackerrank/src/main/java/hackerrank/UtopianTree.java
@@ -1,12 +1,21 @@
 package hackerrank;
 
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 
 public class UtopianTree {
-	private Predicate<Integer> isSpring = cycle -> cycle % 2 != 0; 
-
+	private static Predicate<Integer> isSpring = cycle -> cycle % 2 != 0; 
+	private static Predicate<Integer> isSummer = isSpring.negate();
+	
+	private static Map<Predicate<Integer>, UnaryOperator<Integer>> criteriaToHeightComputer = new HashMap<Predicate<Integer>, UnaryOperator<Integer>>();
+	static {
+		criteriaToHeightComputer.put(isSpring, height -> height * 2);
+		criteriaToHeightComputer.put(isSummer, height -> height + 1);
+	}
+	
 	public int projectedHeight(int cycles) {
 		return IntStream
 				.range(0, cycles + 1)
@@ -15,11 +24,12 @@ public class UtopianTree {
 				});
 	}
 	
-	private Function<Integer, Integer> getHeightComputer(int cycle) {
-		if(isSpring.test(cycle)) {
-			return height -> height * 2;
-		} else {
-			return height -> height + 1;
-		}
+	private UnaryOperator<Integer> getHeightComputer(int cycle) {
+		return criteriaToHeightComputer.keySet()
+							.stream()
+							.filter(cyclePredicate -> cyclePredicate.test(cycle))
+							.map(cycePredicate -> criteriaToHeightComputer.get(cycePredicate))
+							.findFirst()
+							.get();
 	}
 }
