e7a6c7e445aaf7a66e59982d28fe8718ff8b2c5a
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-23 21:19:01 +0530

    refactor printCornerPoints into smaller methods

--- hackerrank/src/main/java/problems/MatrixSpiralPrinter.java
+++ hackerrank/src/main/java/problems/MatrixSpiralPrinter.java
@@ -14,29 +14,44 @@ public class MatrixSpiralPrinter {
 		return sb.toString();
 	}
 
-	public void printCornerPoints(int[][] input, StringBuilder sb, int x1, int y1, int x2, int y2) {
+	protected void printCornerPoints(int[][] input, StringBuilder sb, int x1, int y1, int x2, int y2) {
+		printTopRow(input, sb, x1, y1, y2);
+		printRigthColumn(input, sb, x1, x2, y2);
+		if (x2 > x1) {
+			printBottomRow(input, sb, y1, x2, y2);
+		}
+		if (y2 > y1) {
+			printLeftColumn(input, sb, x1, y1, x2);
+		}
+	}
+
+	private void printTopRow(int[][] input, StringBuilder sb, int x1, int y1, int y2) {
 		for (int i = y1; i <= y2; i++) {
 			sb.append(input[x1][i])
 			  .append(" ");
 		}
-		
+	}
+	
+	private void printBottomRow(int[][] input, StringBuilder sb, int y1, int x2, int y2) {
+		for (int i = (y2 - 1); i >= y1; i--) {
+			sb.append(input[x2][i])
+			  .append(" ");
+		}
+	}
+	
+	private void printLeftColumn(int[][] input, StringBuilder sb, int x1, int y1, int x2) {
+		for (int i = (x2 - 1); i > x1; i--) {
+			sb.append(input[i][y1])
+			  .append(" ");
+		}
+	}
+
+	
+
+	private void printRigthColumn(int[][] input, StringBuilder sb, int x1, int x2, int y2) {
 		for (int i = (x1 + 1); i <= x2; i++) {
 			sb.append(input[i][y2])
 			  .append(" ");
-		}
-
-		if (x2 > x1) {
-			for (int i = (y2 - 1); i >= y1; i--) {
-				sb.append(input[x2][i])
-				  .append(" ");
-			}
-		}
-
-		if (y2 > y1) {
-			for (int i = (x2 - 1); i > x1; i--) {
-				sb.append(input[i][y1])
-				  .append(" ");
-			}
 		}
 	}
 }
--- hackerrank/src/test/java/problems/MatrixSpiralPrinterTest.java
+++ hackerrank/src/test/java/problems/MatrixSpiralPrinterTest.java
@@ -24,20 +24,23 @@ public class MatrixSpiralPrinterTest {
 	 		  				  {13, 14, 15},
 	 		  				  {16, 17, 18}};
 
-
 	
 	@Test
-	public void shouldPrintTheCornerElementsOfInput() {
-		assertPrintingCorner(input, new StringBuilder(), 0, 0, 3, 3, "1 2 3 4 8 12 16 15 14 13 9 5 ");
-		assertPrintingCorner(input , new StringBuilder(), 1, 0, 3, 3, "5 6 7 8 12 16 15 14 13 9 ");
-		assertPrintingCorner(input , new StringBuilder(), 1, 1, 2, 2, "6 7 11 10 ");
-		
-		assertPrintingCorner(input1, new StringBuilder(), 1, 1, 1, 4, "8 9 10 11 "); 
-		assertPrintingCorner(input2, new StringBuilder(), 1, 1, 4, 1, "5 8 11 14 "); 
+	public void shouldPrintTheCornerElementsOfInputForSquareMatrix() {
+		assertPrintingCorner(input, 0, 0, 3, 3, "1 2 3 4 8 12 16 15 14 13 9 5 ");
+		assertPrintingCorner(input , 1, 0, 3, 3, "5 6 7 8 12 16 15 14 13 9 ");
+		assertPrintingCorner(input , 1, 1, 2, 2, "6 7 11 10 ");
 	}
 
 	@Test
-	public void shouldPrintSpirallyForSquareatrix() {
+	public void shouldPrintTheCornerElementsOfInputForRectangleMatrix() {
+		assertPrintingCorner(input1, 1, 1, 1, 4, "8 9 10 11 "); 
+		assertPrintingCorner(input2, 1, 1, 4, 1, "5 8 11 14 "); 
+	}
+
+	
+	@Test
+	public void shouldPrintSpirallyForSquareMatrix() {
 		String exp = "1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 ";
 		Assert.assertEquals(exp, printer.print(input));
 	}
@@ -46,10 +49,14 @@ public class MatrixSpiralPrinterTest {
 	public void shouldPrintSpirallyForRectangleMatrix() {
 		String exp = "1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11 ";
 		Assert.assertEquals(exp, printer.print(input1));
+		
+		exp = "1 2 3 6 9 12 15 18 17 16 13 10 7 4 5 8 11 14 ";
+		Assert.assertEquals(exp, printer.print(input2));
 	}
 	
 
-	private void assertPrintingCorner(int[][] i, StringBuilder sb, int x1, int y1, int x2, int y2, String exp) {
+	private void assertPrintingCorner(int[][] i, int x1, int y1, int x2, int y2, String exp) {
+		StringBuilder sb = new StringBuilder();
 		printer.printCornerPoints(i, sb, x1, y1, x2, y2);
 		Assert.assertEquals(exp, sb.toString());
 	}
