d50fc116b0c3ba4a2867818fe7ba0ef70d990452
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-23 10:26:29 +0530

    Tweak Utopian Tree problem iteration logic to remove initial default height of 1

--- hackerrank/src/main/java/hackerrank/UtopianTree.java
+++ hackerrank/src/main/java/hackerrank/UtopianTree.java
@@ -1,11 +1,12 @@
 package hackerrank;
 
+
 public class UtopianTree {
 
 	public int projectedHeight(int cycles) {
-		int height = 1;
-		for (int i = 1; i <= cycles; i++) {
-			if (i%2 != 0) {
+		int height = 0;
+		for (int i = 0; i <= cycles; i++) {
+			if (i % 2 != 0) {
 				height += height;
 			} else {
 				height += 1;
