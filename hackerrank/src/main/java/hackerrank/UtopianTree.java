1e0754b3d676ee273b87d70a3d01423f6e4458d3
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-23 11:11:30 +0530

    Utopian tree convert getHeightComputer to a method

--- hackerrank/src/main/java/hackerrank/UtopianTree.java
+++ hackerrank/src/main/java/hackerrank/UtopianTree.java
@@ -6,21 +6,20 @@ import java.util.stream.IntStream;
 
 public class UtopianTree {
 	private Predicate<Integer> isSpring = cycle -> cycle % 2 != 0; 
-	private Function<Integer, Function<Integer, Integer>> getHeightComputer =
-				(cycle) -> {
-					if(isSpring.test(cycle)) {
-						return current -> current * 2;
-					} else {
-						return current -> current + 1;
-					}
-				};
 
 	public int projectedHeight(int cycles) {
 		return IntStream
 				.range(0, cycles + 1)
 				.reduce(0, (height, cycle) -> {
-							return getHeightComputer.apply(cycle)
-													.apply(height);
+							return getHeightComputer(cycle).apply(height);
 				});
+	}
+	
+	private Function<Integer, Integer> getHeightComputer(int cycle) {
+		if(isSpring.test(cycle)) {
+			return height -> height * 2;
+		} else {
+			return height -> height + 1;
+		}
 	}
 }
