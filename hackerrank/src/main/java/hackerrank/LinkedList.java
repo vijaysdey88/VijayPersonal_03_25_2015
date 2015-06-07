4dd6cd4d4730b37f8da18bdf97a91fdd58e99eb7
vijaysdey88 <vijaysdey88@gmail.com>
2015-06-07 07:42:05 +0530

    Reverse linked list iterative and recursive

--- hackerrank/src/main/java/hackerrank/LinkedList.java
+++ hackerrank/src/main/java/hackerrank/LinkedList.java
@@ -1,20 +1,33 @@
 package hackerrank;
 
 public class LinkedList {
-	
 	Node head;
-	Node tail;
 	
 	public LinkedList add(int value) {
-		Node node = new Node(value, null);
+		Node newNode = new Node(value, null);
 		if(null == head) {
-			head = node;
-			tail = node;
+			head = newNode;
 		} else {
-			tail.next = node;
-			tail = node;
+			Node temp = head;
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
 		}
 		return this;
+	}
+	
+	public int getNodeFromFront(int n) {
+		Node node = head;
+		
+		while(n > 0 && node != null) {
+			node = node.next;
+			--n;
+		}
+		
+		if(node == null) 
+				throw new IndexOutOfBoundsException("Index invalid - " + n); 
+		return node.data;
 	}
 	
 	/*
@@ -26,16 +39,16 @@ public class LinkedList {
 		// This is a "method-only" submission.
 		// You only need to complete this method.
 
-		Node node = GetIndexFromBack(head, new Counter(n));
+		Node node = getIndexFromBack(head, new Counter(n));
 		return node.data;
 	}
 
-	Node GetIndexFromBack(Node node, Counter c) {
+	Node getIndexFromBack(Node node, Counter c) {
 		Node matched = null;
 		if (node == null) {
 			matched = null;
 		} else {
-			matched = GetIndexFromBack(node.next, c);
+			matched = getIndexFromBack(node.next, c);
 			if (c.count == -1) {
 				matched = node;
 			}
@@ -74,4 +87,41 @@ public class LinkedList {
 	public String toString() {
 		return "LinkedList [head=" + head  + "]";
 	}
+
+	public void reverseIteratively() {
+		if (head != null) {
+			Node previous = null;
+			Node current = head;
+			Node next = head.next;
+			while (null != next) {
+				current.next = previous;
+				Node temp = next.next;
+				next.next = current;
+
+				previous = current;
+				current = next;
+				next = temp;
+			}
+			head = current;
+		}
+	}
+
+	public void reverseRecursively() {
+		if(null != head)
+			head = reverseRecursively(head);
+	}
+	
+	private Node reverseRecursively(Node node) {
+		if(node.next == null) {
+			return node;
+		} else {
+			Node newHead = reverseRecursively(node.next);
+			Node next = node.next;
+			next.next = node;
+			node.next = null;
+			return newHead;
+		}
+	}
+
+
 }
--- hackerrank/src/test/java/hackerrank/LinkedListTest.java
+++ hackerrank/src/test/java/hackerrank/LinkedListTest.java
@@ -1,19 +1,118 @@
 package hackerrank;
 
 
+import hackerrank.LinkedList.Node;
+
+import java.util.stream.IntStream;
+
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class LinkedListTest {
-	private LinkedList ll = new LinkedList();
+	private LinkedList list = new LinkedList();
+	
+	@Before
+	public void setup() {
+		addElementsToList(3);
+	}
+
+
+	private void addElementsToList(int size) {
+		list = new LinkedList();
+		IntStream.range(0, size)
+				 .forEach(list::add);
+	}
+	
+	@Test
+	public void shouldAddNodesInorderOfInsertion() {
+		Node node = list.head;
+		int i = 0;
+		while(node != null) {
+			Assert.assertEquals(i, node.data);
+			i++;
+			node = node.next;
+		}
+	}
 	
 	@Test
 	public void getNthNodeFromBackShouldReturnTheCorrectNode() {
-		ll.add(0).add(1).add(2);
-		
-		Assert.assertEquals(2, ll.getNodeFromBack(ll.head, 0));
-		Assert.assertEquals(1, ll.getNodeFromBack(ll.head, 1));
-		Assert.assertEquals(0, ll.getNodeFromBack(ll.head, 2));
-		
+		Assert.assertEquals(2, list.getNodeFromBack(list.head, 0));
+		Assert.assertEquals(1, list.getNodeFromBack(list.head, 1));
+		Assert.assertEquals(0, list.getNodeFromBack(list.head, 2));
 	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void getNodeFromFrontShouldThrowExceptionWhenListIsEmpty() {
+		list = new LinkedList();
+		list.getNodeFromFront(0);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void getNodeFromFrontShouldThrowExceptionForOutOfBoundsIndex() {
+		list.getNodeFromFront(5);
+	}
+	
+	
+	@Test
+	public void getNthNodeFromFront() {
+		IntStream.range(0, 3)
+				 .forEach(i -> Assert.assertEquals(i, list.getNodeFromFront(i)));
+	}
+	
+	@Test
+	public void shouldNotReverseIterativelyForEmpty() {
+		list = new LinkedList();
+		list.reverseIteratively();
+		Assert.assertNull(list.head);
+	}
+	
+	@Test
+	public void shouldReverseIterativelyForSingleElement() {
+		list = new LinkedList();
+		list.add(0);
+		list.reverseIteratively();
+		assertElementsInList(0);
+	}
+	
+	@Test
+	public void shouldReverseListUsingIterationForManyNodes() {
+		addElementsToList(5);
+		list.reverseIteratively();
+		assertElementsInList(4, 3, 2, 1, 0);
+	}
+	
+	
+
+	@Test
+	public void shouldNotReverseRecursivelyForEmpty() {
+		list = new LinkedList();
+		list.reverseRecursively();
+		Assert.assertNull(list.head);
+	}
+	
+	@Test
+	public void shouldReverseRecursivelyForSingleElement() {
+		list = new LinkedList();
+		list.add(0);
+		list.reverseRecursively();
+		assertElementsInList(0);
+	}
+	
+	@Test 
+	public void shouldReverseListUsingRecursionForManyNodes() {
+		addElementsToList(5);
+		list.reverseRecursively();
+		assertElementsInList(4, 3, 2, 1, 0);
+	}
+
+	private void assertElementsInList(int... expected) {
+		IntStream.range(0, expected.length)
+				 .forEach(i -> Assert.assertEquals("For i = " + i , expected[i], list.getNodeFromFront(i)));
+	}
+	
+	
+	
+	
 }
